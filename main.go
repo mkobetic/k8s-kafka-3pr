eb0395f4f68433cf775fbcc684f9c9d2ac429160
Martin Kobetic <mkobetic@gmail.com>
2016-10-25 15:57:29 -0400

    maybe working?

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/mkobetic/k8s-kafka-3pr/types"
+
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/api/unversioned"
+	"k8s.io/client-go/pkg/runtime/serializer"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	kubeconfig = flag.String("kubeconfig", "$HOME/.kube/config", "absolute path to the kubeconfig file")
+)
+
+func main() {
+	flag.Parse()
+	resource := flag.Arg(0)
+	// uses the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", os.ExpandEnv(*kubeconfig))
+	if err != nil {
+		panic(err.Error())
+	}
+	config.APIPath = "/apis"
+	config.GroupVersion = &unversioned.GroupVersion{Group: "shopify.io", Version: "v1"}
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: api.Codecs}
+	client, err := rest.RESTClientFor(config)
+	if err != nil {
+		panic(err)
+	}
+	// topics, err := clientset.Extensions().ThirdPartyResources().Get("kafka-topic.shopify.io")
+
+	r := client.Get().Resource(resource)
+	fmt.Println(r.URL())
+	topics, err := r.Do().Get()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(topics.(*types.KafkaTopicList))
+}
---
+++ types/register.go
@@ -0,0 +1,72 @@
+package types
+
+import (
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/api/unversioned"
+	"k8s.io/client-go/pkg/apimachinery/announced"
+	"k8s.io/client-go/pkg/runtime"
+	"k8s.io/client-go/pkg/util/sets"
+)
+
+func init() {
+	if err := announced.NewGroupMetaFactory(
+		&announced.GroupMetaFactoryArgs{
+			GroupName:                  GroupName,
+			VersionPreferenceOrder:     []string{GroupVersion.Version},
+			ImportPrefix:               "github.com/mkobetic/k8s-kafka-3pr/types",
+			RootScopedKinds:            sets.NewString("ThirdPartyResource"),
+			AddInternalObjectsToScheme: AddToScheme,
+		},
+		announced.VersionToSchemeFunc{
+			GroupVersion.Version: AddToScheme,
+		},
+	).Announce().RegisterAndEnable(); err != nil {
+		panic(err)
+	}
+}
+
+// GroupName is the group name use in this package
+const GroupName = "shopify.io"
+
+// GroupVersion is group version used to register these objects
+var GroupVersion = unversioned.GroupVersion{Group: GroupName, Version: "v1"}
+
+// Kind takes an unqualified kind and returns a Group qualified GroupKind
+func Kind(kind string) unversioned.GroupKind {
+	return GroupVersion.WithKind(kind).GroupKind()
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) unversioned.GroupResource {
+	return GroupVersion.WithResource(resource).GroupResource()
+}
+
+var (
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
+// Adds the list of known types to api.Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	// TODO this gets cleaned up when the types are fixed
+	scheme.AddKnownTypes(GroupVersion,
+		&KafkaTopicList{},
+		&KafkaTopic{},
+	)
+	return nil
+}
+
+type KafkaTopicList struct {
+	unversioned.TypeMeta `json:",inline"`
+
+	// Standard list metadata.
+	unversioned.ListMeta `json:"metadata,omitempty"`
+
+	Items []KafkaTopic `json:"items"`
+}
+
+type KafkaTopic struct {
+	unversioned.TypeMeta `json:",inline"`
+	// Standard object metadata; More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#metadata.
+	api.ObjectMeta `json:"metadata,omitempty"`
+}
